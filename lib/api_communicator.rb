05249a67d0b53788454605e3d1ee29b14928c84e
Yomi <alajide@gmail.com>
2016-11-30 14:50:16 -0500

    Updated code to include .lowercase

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,64 +7,30 @@ def character_hash
   all_characters = RestClient.get("http://www.swapi.co/api/people/")
   hash = JSON.parse(all_characters)
 end
-# => entire hash
-# puts character_hash
+#=> all characters hash
 
 
 def get_character_movies_from_api(character)
   chosen_one = character_hash["results"].find do |characters|
-     characters["name"] == character
-    end
-    # => [urls]
-    #make the web request
-
-    chosen_one["films"].collect do |films_url|
-      all_films = RestClient.get(films_url)
-       JSON.parse(all_films)
-    end
-    # return films_info.is_a?(Array) # true
+   characters["name"].downcase == character
   end
-
-
-
-# p get_character_movies_from_api("R2-D2")
-#MAKE WEB request
-
-
-
-
+  # => single character hash
+  #make the web request
+  chosen_one["films"].sort.collect do |films_url|
+    all_films = RestClient.get(films_url)
+    JSON.parse(all_films)
+  end
+end
 
 
 def parse_character_movies(character)
   # some iteration magic and puts out the movies in a nice list
-
   film_info = get_character_movies_from_api(character).collect do |key, value| #each movie
     "Movie title:#{key["title"]} Producer(s): #{key["producer"]} Episode No.:#{key["episode_id"]}"
   end
-
-   puts film_info[0...film_info.size]
-
+    puts film_info #[0...film_info.size]
 end
 
-parse_character_movies("Luke Skywalker")
-
-
-# .each do |key, value|
-#
-#      puts ["title"][value]
-#       puts["episode_id"][value]
-#       puts ["director"][value]
-#       binding.pry
-# end
-
-
-
-
-
-
-# def parse_character_movies(films_hash)
-#   # some iteration magic and puts out the movies in a nice list
-# end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
@@ -75,3 +41,16 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+# we did it :)
+
+
+# def get_character_movies_from_api(character)
+#   hash = {}
+#   character_hash["results"].each do |stats|
+#      if stats["name"].downcase == character
+#        hash = stats
+#        binding.pry
+#       end
+#
+#     end
